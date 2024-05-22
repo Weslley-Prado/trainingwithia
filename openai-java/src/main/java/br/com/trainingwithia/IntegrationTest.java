77fd0b32b8cdf42892e04acbe7840e45f663c0df
Weslley-Prado <weslley.prado73@gmail.com>
2024-05-22 09:41:58 -0300

    feat/main: create integration test

--- openai-java/src/main/java/br/com/trainingwithia/IntegrationTest.java
+++ openai-java/src/main/java/br/com/trainingwithia/IntegrationTest.java
@@ -5,4 +5,5 @@ public class IntegrationTest {
         var user = "Gere 5 produtos";
         var system = "Você é um gerador de produtos fictícios para um ecommerce e deve gerar apenas o nome dos produtos solicitados pelo usuário";
     }
+
 }
